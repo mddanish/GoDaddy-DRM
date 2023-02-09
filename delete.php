2434241be518194a908b038d0f380deed06722a3
Mohammed Danish Amber <me@mohammeddanishamber.com>
2023-02-09 15:18:27 +0530

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Change DNS Record</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  </head>
+  <body>
+    <div class="container" style="margin-top: 50px;">
+      
+
+<?php
+require_once 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Delete the DNS record
+    $url = "https://api.godaddy.com/v1/domains/" . $domain . "/records/" . $_POST['type'] . "/" . $_POST['name'];
+    
+    $ch = curl_init();
+    
+    curl_setopt_array($ch, [
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => [
+            "Authorization: sso-key " . $apiKey . ":" . $apiSecret,
+            "Accept: application/json",
+            "Content-Type: application/json"
+        ],
+        CURLOPT_CUSTOMREQUEST => "DELETE"
+    ]);
+    
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    
+    curl_close($ch);
+    
+    // Check if the API call was successful
+    if ($httpCode == 200) {
+        echo "The DNS record was deleted successfully.";
+    } else {
+        echo "An error occurred while deleting the DNS record.";
+    }
+} else {
+    // Display the form to delete a DNS record
+    echo "<center><h1>Delete DNS Records</h1></center>";
+    echo "
+        <form method='post'>
+          <div class='form-group'>
+            <label>Type:</label>
+            <input type='text' class='form-control' name='type' required>
+          </div>
+          <div class='form-group'>
+            <label>Name:</label>
+            <input type='text' class='form-control' name='name' required>
+          </div>
+          <div class='form-group'>
+            <label>Value:</label>
+            <input type='text' class='form-control' name='value' required>
+          </div>
+          <input type='submit' class='btn btn-danger' value='Delete'>
+        </form>
+    </div>
+  </body>
+</html>
+";
+}
+?>
